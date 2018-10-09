78a61e8ef60ee81b9705169257d086f8ad72dda5
Ruby Rubenstahl <ruby@ruby-lighting.com>
2018-10-09 12:54:04 -0400

    Add more tests

--- rangeMap.test.js
+++ rangeMap.test.js
@@ -1,4 +1,24 @@
-const rangeMap = require('./index');
+const rangeMap = require("./index");
+
+test("scales 50 from range 0-100 to 500 in range 0-1000", () => {
+  expect(rangeMap(50, 0, 100, 0, 1000)).toBe(500);
+});
+
+test("scales 0 from range 0-100 to 0 in range 0-1000", () => {
+  expect(rangeMap(0, 0, 100, 0, 1000)).toBe(0);
+});
+
+test("scales 8 from range 0-8 to 1100 in range 0-1000 when not clamped", () => {
+  expect(rangeMap(8, 0, 8, 0, 1100)).toBe(1100);
+});
+
+test("scales 110 from range 0-100 to 1100 in range 0-1000 when not clamped", () => {
+  expect(rangeMap(110, 0, 100, 0, 1000)).toBe(1100);
+});
+
+test("scales -15 from range 0-100 to -150 in range 0-1000 when not clamped", () => {
+  expect(rangeMap(-15, 0, 100, 0, 1000)).toBe(-150);
+});
 
 test('scales 50 from range 0-100 to 500 in range 0-1000', () => {
   expect(rangeMap(50, 0, 100, 0, 1000)).toBe(500)
